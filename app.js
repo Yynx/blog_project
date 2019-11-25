9529e0d0ef22e52b4d6af57b267c58e821d89472
chipembere <chitiyobrian@gmail.com>
2019-11-25 15:02:45 +0000

    Added json file reading and appending

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
+//imported files 
 const fs = require('fs');
 const express = require('express');
+const ejs = require('ejs');
+
+//setup for express
+const app = express();
+const port = 3000;
+app.engine('html', ejs.renderFile);
+app.use(express.static('public'));
+app.use(express.static('views'));
+app.use(express.urlencoded());
 
 
-fs.writeFile("formData.json", data, function (err) {
-    if (err) {
-        console.log(err);
-    }
+//saves new json object to array
+const saveJson = (jsonObj) => {
+    fs.readFile('public/blogs.json', (err, data) => {
+        if (err) throw err;
+        let testData = JSON.parse(data);
+        testData.push(jsonObj);
+        tDJson = JSON.stringify(testData);
+        fs.writeFile("public/blogs.json", tDJson, () => {
+        });
+    });
+}
+
+//first page render
+app.get('/', (req, res) => {
+    res.render('blog.html');
 });
+
+//when form data is submitted
+app.post('/submit', (req, res) => {
+    let title = req.body.title;
+    let content = req.body.content;
+    let data = { title: title, content: content };
+    saveJson(data);
+    res.redirect('/');
+});
+
+app.listen(port, () => console.log(`Listening on ${port}`));
+
+
--- public/blog.js
+++ public/blog.js
@@ -1,29 +1,12 @@
 $(document).ready(() => {
-    //console.log("formData");
-    // $('form').submit((event) => {
-    // event.preventDefault();
-    // let formData = $('form').serializeArray();
-    // console.log(formData);
-    // let title = formData[0].name;
-    // let titleVal = formData[0].value;
-    // let content = formData[1].name;
-    // let contentVal = formData[1].value;
-    // let jsonFormData = { title: titleVal, content: contentVal }
-    // console.log(`JSON ${jsonFormData}`);
-    $.getJSON("formData.json", function (data) {
-        console.log(data);
-        // let dataJson = JSON.stringify(data);
-        // console.log(dataJson);
+    console.log("Page Ready.")
+    
+    //Logs current blog data
+    $.getJSON("blogs.json", function (data) {
+        let dataJson = JSON.stringify(data);
+        console.log('DAta' + dataJson);
     });
 
     // $('#formOutput').html(`Title: ${jsonFormData.title} 
     // Content: ${jsonFormData.content}`);
-
-
-    // axios.post('/submit', {
-    //     firstName: 'Fred',
-    //     lastName: 'Flintstone'
-    // });
-
-    // });
 });
