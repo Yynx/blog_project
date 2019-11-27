46a599e649c736a3d7ca98cb1fa8c8189a624f31
Yynx <47715709+Yynx@users.noreply.github.com>
2019-11-27 10:55:59 +0000

    updated blogPage css

--- public/js/form.js
+++ public/js/form.js
@@ -2,6 +2,7 @@ $(document).ready(() => {
     console.log($('#inputBlogAuthor').val().length);
 
     let maxLength = 1200;
+    // currently character count does not work for emojis
     $('#inputBlogPost').keyup(() => {
         let textLength = maxLength - $('#inputBlogPost').val().length;
         $('#remainingChars').text(textLength);
