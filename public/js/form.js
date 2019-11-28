7bfc81a8151efa59efbc8b56a4b57ca4f6532a49
Yynx <47715709+Yynx@users.noreply.github.com>
2019-11-28 17:49:12 +0000

    added gif selection functionality

--- public/js/form.js
+++ public/js/form.js
@@ -12,49 +12,68 @@ $(document).ready(() => {
     // GIPHY API starts 
 
     let ApiKey = "5u1gTu3XcLrgMWBSwrhb1uwYohwVMCNv";
-    getQuery();
-    function getQuery() {
+    
+    $('#text').on('input', function() {
+        $('#imgBox').empty();
+        console.clear()
         let timeout = null;
-        let query = document.getElementById('text');
-        $(query).keyup(function () {
+        $('#text').keyup(function() {
             clearTimeout(timeout);
+            console.clear();
             timeout = setTimeout(function () {
                 let input = $('#text').val();
-                let inLength = input.length;
-                if(inLength > 0) {
-                    gifFetch(input)
-                }
-                else {
+                console.clear();
+                console.log(input)
+                if(input.length === 0) {
                     $('#imgBox').empty();
-                }
-            },200)
-        })
-    }
-    function gifFetch(input) {
-        const result = fetch(`http://api.giphy.com/v1/gifs/search?q=${input}&limit=20&api_key=${ApiKey}`).then( response => response.json()).then(data => {
-            let gUrl = data.data;
-            // console.log(gUrl);
-            // let gUrl = data.data[0].id;
-            // let nuUrl = `https://media.giphy.com/media/${gUrl}/giphy.gif`;
-            // gAppend(nuUrl);
-            linkMkr(gUrl)
-        })
-    }
-    function linkMkr(gUrl) {
-        let lnkList = []
-        for(x = 0; x < 20; x++){
-            let nuUrl = `https://media.giphy.com/media/${gUrl[x].id}/giphy.gif`;
-            lnkList.push(nuUrl)
-        };
-        gAppend(lnkList)
-    }
-    function gAppend(lnkList) {
-        for(x = 0; x < 20; x++) {
-            $('#imgBox').append(`<img class="gif" src='${lnkList[x]}'>`);
-        }
-    }
+                } //if statement
+                else {
+                    let txt = $('#text').val();
+                    gifFetch(txt)
+                } // else statement
+            }, 500) //timeout = setTimeout
+            function gifFetch(txt) {
+                const result = fetch(`http://api.giphy.com/v1/gifs/search?q=${txt}&limit=20&api_key=${ApiKey}`)
+                .then( response => response.json())
+                .then(data => {
+                let gUrl = data.data;
+                linkMkr(gUrl);
+                })
+                .then(data => {//event listeners
+                    let gifs = document.getElementsByClassName('gif')
+                    console.log(gifs);
+                    for (gif of gifs)
+                    {   
+                        gif.addEventListener('click', function(event) {
+                            $('.gif').css('border','none')
+                            $(event.target).css('border', '5px solid purple')
+                            let url = event.target.src
 
+                            let gifForm = $('#gifLink').val(url);
+                
+                    })
+                    }
+                    
+                   
+                })
+            } //gifFetch
+            function linkMkr(gUrl) {
+                let lnkList = []
+                for(x in gUrl){
+                    let nuUrl = `https://media.giphy.com/media/${gUrl[x].id}/giphy.gif`;
+                    lnkList.push(nuUrl)
+                };
+                gAppend(lnkList);
+            } //linkMkr
+            function gAppend(lnkList) {
+                for(x in lnkList) {
+                    $('#imgBox').append(`<img class="gif" src='${lnkList[x]}' alt="can't load gif">`);
+                }} //gAppend
+               
+        }) // text.keyup
+    }) //text.on('input')
 
+   
 
 
 });
